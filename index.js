dc732d1f562e46ff9d0013e6386a011d936ef8ab
Aslam Rasheed <aslampr07@outlook.com>
2021-04-24 14:20:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const got = require('got');
+const cheerio = require('cheerio');
+const http = require('http');
+const WebSocket = require('ws');
+
+
+async function getTotal() {
+    try {
+        const result = await got("https://donation.cmdrf.kerala.gov.in/index.php/Dashboard/allType_transaction").text()
+        const $ = cheerio.load(result);
+        const tbody = $('#dashboard > div:nth-child(4) > table > tbody > tr')
+        let final = 0;
+        tbody.each((i, e) => {
+            if (i > 2 && i != 27) {
+                const tds = $(e).find("td")
+                const amount = $(tds[4]).text()
+                final = final + parseInt(amount)
+            }
+        })
+        return final
+    } catch (error) {
+        console.log("Error", error)
+    }
+}
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server })
+
+wss.on("connection", (ws, req) => {
+    console.log("Client Connected: ")
+
+})
+
+server.listen(8000, () => {
+    console.log("Server is listerning")
+})
+
+setInterval(async () => {
+    const result = await getTotal()
+    console.log(result)
+    broadCastData(JSON.stringify({ todayTotal: result }))
+}, 6000);
+
+
+function broadCastData(data) {
+    wss.clients.forEach(client => {
+        client.send(data)
+    })
+}
